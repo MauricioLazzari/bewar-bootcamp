39cd483774ad98cd632abe0fa227356edafbb0c0
Maurício Lazzari <codelazzari@gmail.com>
2025-10-23 23:50:10 -0300

    Update shadcn theme
    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
     <div>
-      <Button className="cursor-pointer bg-red-50">Button</Button>
+      <Button className="cursor-pointe">Button</Button>
       <h1>Home</h1>
     </div>
   );
